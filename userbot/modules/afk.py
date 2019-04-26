57305aa7df42e77324b643a9c950500dd0ae461e
Ankur Singh <42770572+ankicing@users.noreply.github.com>
2019-04-26 21:14:38 +0530

    Update afk.py

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -23,8 +23,8 @@ async def mention_afk(mention):
         if ISAFK:
             if mention.sender_id not in USERS:
                 await mention.reply(
-                    f"Sorry! My boss is AFK due to `{AFKREASON}`."
-                    "\nWould ping him to look into the message soon 😉."
+                    f"I'm busy right now."
+                    "\n🏃."
                 )
                 USERS.update({mention.sender_id: 1})
                 COUNT_MSG = COUNT_MSG + 1
